550c23d05b5ef6c53ac1ade99e5158c73e760525
ngatngay <ngatngay@ngatngay.net>
2024-07-06 15:26:20 +0700

    #1

---
+++ README.md
@@ -0,0 +1,22 @@
+# Linux File Manager
+
+## File Manager
+
+https://github.com/ngatngay/file-manager
+
+## Install
+
+- for root user
+- for systemd
+
+```
+curl -s https://raw.githubusercontent.com/ngatngay/linux-file-manager/main/linux-file-manager.sh | bash
+```
+
+## Run
+
+Open port 9753 first:
+
+`http://[IP_VPS]:9753`
+
+
---
+++ linux-file-manager.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+DIR=~/ngatngay-file-manager
+ADDR="0.0.0.0:9753"
+
+apt install -y wget php-cli php-common php-mbstring php-xml php-mysql php-curl php-zip
+
+# service
+cat << EOF > /etc/systemd/system/ngatngay-file-manager.service
+[Unit]
+Description=ngatngay File Manager
+After=network.target
+
+[Service]
+ExecStart=/bin/bash -c 'php -S $ADDR -t $DIR >& /dev/null'
+Restart=always
+User=root
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl stop ngatngay-file-manager
+systemctl start ngatngay-file-manager
+systemctl enable ngatngay-file-manager
+
+mkdir -p $DIR
+cd $DIR
+
+wget https://github.com/ngatngay/file-manager/releases/latest/download/file-manager-full.zip -O file-manager.zip
+unzip -o file-manager.zip
+
+# chmod -R 755 $DIR
+
+echo ""
+echo "Install OK! Access [IP]:9753 to use!"
+
